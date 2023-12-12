3fccea69b9449e802a1e2e8c4399954809fcb82d
Yuxi Liu <yuxi.liu.1995@gmail.com>
2023-12-11 17:14:16 -0800

    wigner rotation post wip

--- blog/posts/wigner-rotation/2d_p5js/script.js
+++ blog/posts/wigner-rotation/2d_p5js/script.js
@@ -1,69 +1,43 @@
-// First sketch for the 3-velocity
-let sketch1 = function(p) {
-    let threeVelocity;
-    let circleRadius;
 
-    p.setup = function() {
-        p.createCanvas(400, 400);
-        circleRadius = p.width / 2 - 10;
-        threeVelocity = new DraggablePoint(p, p.width / 2, p.height / 2);
-    };
+let lineLength = 100;
+let r = 0.1;
+let lineNumber = 20;
 
-    p.draw = function() {
-        p.background(0);
-        p.ellipse(p.width / 2, p.height / 2, circleRadius * 2, circleRadius * 2); // circle
-        threeVelocity.update();
-        threeVelocity.display();
-    };
+let rSlider, lineNumberSlider;
 
-    class DraggablePoint {
-        constructor(p, x, y) {
-            this.p = p;
-            this.x = x;
-            this.y = y;
-            this.dragging = false;
-            this.radius = 10;
-        }
+function setup() {
+  let canvas = createCanvas(300, 300);
+  canvas.parent("canvas")
 
-        update() {
-            if (this.dragging) {
-                this.x = this.p.mouseX;
-                this.y = this.p.mouseY;
-            }
-        }
+  rSlider = createSlider(0.05, 0.2, 0.2, 0.05);
+  rSlider.position(20, 20);
+  lineNumberSlider = createSlider(0, 20, 0, 1);
+  lineNumberSlider.position(20, 50);
+}
+function draw() {
+  background(255);
+  let r = rSlider.value();
+  let lineNumber = lineNumberSlider.value();
 
-        display() {
-            this.p.fill(175);
-            this.p.ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
-        }
-
-        pressed() {
-            let d = this.p.dist(this.p.mouseX, this.p.mouseY, this.x, this.y);
-            if (d < this.radius) {
-                this.dragging = true;
-            }
-        }
-
-        released() {
-            this.dragging = false;
-        }
+  translate(width / 2, height / 2);
+  
+  for (let i = -lineNumber; i <= lineNumber; i++) {
+    push();
+    if (i >= 0) {
+      for (let j = 0; j < i; j++) {
+        applyMatrix(1, r, r, 1, 0, 0);
+      }
+    } else {
+      for (let j = 0; j < -i; j++) {
+        applyMatrix(1, -r, -r, 1, 0, 0);
+      }
     }
+    line(0, -lineLength, 0, lineLength);
+    line(-lineLength, 0, lineLength, 0);
 
-    p.mousePressed = function() {
-        threeVelocity.pressed();
-    };
+    pop();
+  }
 
-    p.mouseReleased = function() {
-        threeVelocity.released();
-    };
-};
-
-// Second sketch for the 4-velocity
-// let sketch2 = function(p) {
-//     // Define variables and methods for the second sketch
-//     // This will be similar to the first one but will include the logic for 4-velocity
-// };
-
-// Initialize the sketches
-new p5(sketch1, "velocity-canvas");
-// new p5(sketch2, "spacetime-canvas");
+  line(-width / 2, 0, width / 2, 0); // x-axis
+  line(0, -height / 2, 0, height / 2); // y-axis
+}
